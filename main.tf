9759b49fd6da2eb19ace1b7dae52808b0f7dbea2
mounika <mounika.gg77@gmail.com>
2023-09-29 15:50:23 +0530

    3 commit

--- main.tf
+++ main.tf
@@ -18,3 +18,27 @@ module "vpc" {
   private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
+
+# create nat gateways
+module "nat_gateway" {
+  source                     = "git@github.com:mounikainfo/Project-Module.git//nat-gateway"
+  project_name               = local.project_name
+  environment                = local.environment
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+}
+
+# create security groups
+module "security_group" {
+  source       = "git@github.com:mounikainfo/Project-Module.git//secutity-groups"
+  project_name = local.project_name
+  environment  = local.environment
+  vpc_id       = module.vpc.vpc_id
+  ssh_ip       = var.ssh_ip
+}
