5d2e4fd88e574ebc4f538a721e052901518b0ac4
mounika <mounika.gg77@gmail.com>
2023-09-30 15:51:30 +0530

    new files

---
+++ dep3.tf
@@ -0,0 +1,49 @@
+resource "kubernetes_deployment" "deployment3" {
+  metadata {
+    name = "webappdeployment3"
+    /* labels = {
+      test = "my-app"
+    } */
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        test = "webapp3"
+      }
+    }
+
+    template {
+      metadata {
+        # name = mypod
+        labels = {
+          test = "webapp3"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.21.6"
+          name  = "nginx-container"
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
