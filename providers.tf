6457627462a47e3f7d9ef29bc8f82f3ab61dc06c
mounika <mounika.gg77@gmail.com>
2023-10-14 15:41:10 +0530

    new

--- providers.tf
+++ providers.tf
@@ -12,7 +12,6 @@ provider "aws" {
   }
 }
 
-
 terraform {
   required_providers {
     kubernetes = {
@@ -23,20 +22,10 @@ terraform {
       source = "hashicorp/helm"
       version = "~> 2.5"
     }
-    http = {
-      source = "hashicorp/http"
-      #version = "2.1.0"
-      version = "~> 2.1"
-    }
-    # ingress-nginx = {
-    #   source  = "ingress-nginx/ingress"
-    #   version = "~> 1.2"  # Replace with the desired version
-    # }
   }
 }
 
 provider "kubernetes" {
-  # Configuration options
   config_path    = "~/.kube/config"
   config_context = "arn:aws:eks:ap-south-1:140382828045:cluster/sta_cluster"
 }
@@ -44,12 +33,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config" 
-    cluster_ca_certificate = "base64decode(arn:aws:iam::140382828045:oidc-provider/oidc.eks.ap-south-1.amazonaws.com/id/B725096B926A9B918A603F2C95B8EC60)"
+    # cluster_ca_certificate = "base64decode(arn:aws:iam::140382828045:oidc-provider/oidc.eks.ap-south-1.amazonaws.com/id/B725096B926A9B918A603F2C95B8EC60)"
   }
 }
 
-provider "ingress" {
-  kubernetes {
-    config_path = "~/.kube/config"  # Path to your Kubernetes config file
-  }
-}
